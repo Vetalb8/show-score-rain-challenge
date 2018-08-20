8b678e5fb947fb0f221dd90d1812145da0fe46b3
Vitaliy Kudla <vkudla@scentbird.com>
2018-08-20 11:58:17 +0400

    added title and image styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 
+import GameField from '../GameField/GameField'
+
+import headerImage from './images/header.png'
+
 import './App.scss'
 
 
@@ -7,7 +11,14 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="app">App</div>
+      <div className="app">
+        <div className="container">
+          <div className="title">
+            <img className="image" src={headerImage} alt="show-score"/>
+          </div>
+          <GameField />
+        </div>
+      </div>
     )
   }
 }
